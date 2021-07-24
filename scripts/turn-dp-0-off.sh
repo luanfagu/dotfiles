c467a3a0956882375849ef2c700105e45901b1e1
Luan Fagundes <luanfagu@gmail.com>
2021-07-24 12:56:58 -0300

    Dotfiles v1.1

--- scripts/turn-dp-0-off.sh
+++ scripts/turn-dp-0-off.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Turn the monitor plugged in DP-0 OFF
-xrandr --output DP-0 --off
+# Turn the monitor plugged in HDMI-0 OFF
+xrandr --output HDMI-0 --off
 
 # Restore nitrogen to fix eventual wallpaper issues
 nitrogen --restore
--- scripts/turn-dp-0-on.sh
+++ scripts/turn-dp-0-on.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Turn the monitor plugged in DP-0 OFF
-xrandr --output DP-0 --auto
-xrandr --output DP-0 --left-of HDMI-0
+xrandr --output HDMI-0 --auto
+xrandr --output HDMI-0 --left-of DP-0
 
 # Restore nitrogen to fix eventual wallpaper issues
 nitrogen --restore
